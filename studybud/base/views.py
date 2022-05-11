bca7b0771be3cdea8a8061a2b4872ba9c3f434cc
Sheersendu <shibrata15@gmail.com>
2022-05-12 01:35:21 +0530

    Created tables for rooms, topics, messages and rendered room host and topic on screen

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -2,22 +2,25 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from .models import Room
 
-rooms = [
-    {'id':1, 'name':'Lets learn Python!'},
-    {'id':2, 'name':'Lets learn Django!'},
-    {'id':3, 'name':'Lets create something new!'}
-]
+# rooms = [
+#     {'id':1, 'name':'Lets learn Python!'},
+#     {'id':2, 'name':'Lets learn Django!'},
+#     {'id':3, 'name':'Lets create something new!'}
+# ]
 def home(request):
+    rooms = Room.objects.all() 
     context = {"rooms":rooms}
     return render(request, 'base/home.html', context)
     # return HttpResponse("HomePage")
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    room = Room.objects.get(id=pk)
+    # room = None
+    # for i in rooms:
+    #     if i['id'] == int(pk):
+    #         room = i
     context = {'room':room}
     return render(request, 'base/room.html',context)
     # return HttpResponse("Room Page")
