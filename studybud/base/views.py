90c97cb0befa1f67f3173e8c5569ce89b3aa341c
Sheersendu <shibrata15@gmail.com>
2022-05-13 02:00:08 +0530

    CRUD for ROOM implemented

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from django.http import HttpResponse
 from .models import Room
+from .forms import RoomForm
 
 # rooms = [
 #     {'id':1, 'name':'Lets learn Python!'},
@@ -24,3 +25,32 @@ def room(request, pk):
     context = {'room':room}
     return render(request, 'base/room.html',context)
     # return HttpResponse("Room Page")
+
+def createRoom(request):
+    form = RoomForm()
+    if request.method == 'POST':
+        # print(request.POST)
+        received_form = RoomForm(request.POST)
+        if received_form.is_valid():
+            received_form.save()
+            return redirect('home')
+    context = {'form':form}
+    return render(request,'base/room_form.html', context)
+
+def updateRoom(request,pk):
+    room = Room.objects.get(id = pk)
+    form = RoomForm(instance=room)
+    if request.method == "POST":
+        received_form = RoomForm(request.POST,instance=room)
+        if received_form.is_valid():
+            received_form.save()
+            return redirect('home')
+    context = {'form':form}
+    return render(request,'base/room_form.html', context)
+
+def deleteRoom(request,pk):
+    room = Room.objects.get(id = pk)
+    if request.method == "POST":
+        room.delete()
+        return redirect('home')
+    return render(request,'base/delete.html',{'obj':room})
